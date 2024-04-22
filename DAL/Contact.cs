012b783ed546974dd8cda99974436b857448ef8f
Masoud Rahimi <masaoud64@yahoo.com>
2024-04-22 21:10:44 +0330

    Create Model Contact and Make DataBase

--- DAL/Contact.cs
+++ DAL/Contact.cs
@@ -11,7 +11,8 @@ namespace DAL
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public string Company { get; set; }
-        public string Description { get; set; }
+        public string? Company { get; set; }
+        public string? Description { get; set; }
+        public DateTime CreationDate { get; set; }
     }
 }
---
+++ DAL/Context/DataBaseContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DAL.Context
+{
+    public class DataBaseContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=.;Initial Catalog=ContactDb;Trusted_Connection=SSPI;Encrypt=false;TrustServerCertificate=true;Persist Security Info=True;User ID=sa;Password=123456");
+        }
+
+        public DbSet<Contact> Contacts { get; set; }
+    }
+}
